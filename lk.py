a52f8a0aefba2c1a58da55aeee37e6d22c2768a3
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2023-04-30 22:48:27 +0530

    Update lk.py

--- lk.py
+++ lk.py
@@ -4,12 +4,12 @@ import time
 
 driver = webdriver.Chrome()
 driver.get("https://www.linkedin.com/login")
-
+time.sleep(5)
 email = driver.find_element_by_id("username")
 email.send_keys("your_email_address")
 time.sleep(5)
 password = driver.find_element_by_id("password")
 password.send_keys("your_password")
-
+time.sleep(5)
 password.send_keys(Keys.RETURN)
 time.sleep(5)
